6a2408a5ead7849401cc1342ad91d3ac1fc8382c
AgusAbaroa2001 <abaroa2@gmail.com>
2024-10-27 16:07:40 -0700

    avancecarrootravez

--- screens/Carrito.tsx
+++ screens/Carrito.tsx
@@ -1,82 +1,111 @@
-import { View, Text, Button, StyleSheet, SafeAreaView, Image, ImageBackground,TouchableOpacity } from 'react-native'
-import React from 'react'
-import { MenuSup } from '../components/MenuSup'
+import React, { useState } from 'react';
+import { View, Text, Button, StyleSheet, SafeAreaView, Image, ImageBackground, TouchableOpacity, Modal } from 'react-native';
+import { MenuSup } from '../components/MenuSup';
 
 export const Carrito = () => {
-    const handlePress = () => {
-        console.log("Botón presionado");
-        // Aquí puedes agregar la funcionalidad que deseas que ocurra al presionar el botón
-      };
-  return (
-    <ImageBackground
-     source={require('../src/img/goku.jpg')}
-     style={styles.imageBack}
-    >
-    <SafeAreaView style = {styles.container}>
+    const [modalVisible, setModalVisible] = useState(false);
 
-    <View style = {styles.textContainer}>
-        <Image
-            source={require('../src/img/carrito.png')}
-            style={styles.image}
-            ></Image>
-    <Text style = {styles.text}>Carrito</Text>
-    </View>
-          
-      <MenuSup/>
-      <TouchableOpacity style={styles.btn} onPress={handlePress}>
-          <Text style={styles.btntext}>PAGAR</Text>
-        </TouchableOpacity>
-    </SafeAreaView>
-    </ImageBackground>
-  )
-}
+    const openModal = () => {
+        setModalVisible(true);
+    };
+
+    const closeModal = () => {
+        setModalVisible(false);
+    };
+
+    return (
+        <ImageBackground source={require('../src/img/goku.jpg')} style={styles.imageBack}>
+            <SafeAreaView style={styles.container}>
+                <View style={styles.textContainer}>
+                    <Image source={require('../src/img/carrito.png')} style={styles.image} />
+                    <Text style={styles.text}>Carrito</Text>
+                </View>
+                <MenuSup />
+
+                
+                <TouchableOpacity style={styles.btn} onPress={openModal}>
+                    <Text style={styles.btntext}>BOTON</Text>
+                </TouchableOpacity>
+
+                
+                <Modal
+                    animationType="slide"
+                    transparent={true}
+                    visible={modalVisible}
+                    onRequestClose={closeModal} 
+                >
+                    <View style={styles.modalBackground}>
+                        <View style={styles.modalContainer}>
+                            <Text style={styles.modalText}>USTED PRESIONO EL BOTON</Text>
+                            <Button title="Cerrar" onPress={closeModal} />
+                        </View>
+                    </View>
+                </Modal>
+            </SafeAreaView>
+        </ImageBackground>
+    );
+};
 
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        justifyContent: 'flex-start'
-        
+        justifyContent: 'flex-start',
     },
     text: {
         flex: 2,
         marginTop: 40,
         marginLeft: 10,
-        fontSize: 24
+        fontSize: 24,
     },
     textContainer: {
-        flexDirection: "row",
-        alignItems: "center",
-        height: "10%",
-        width: "100%",
-        padding: 10
-        
+        flexDirection: 'row',
+        alignItems: 'center',
+        height: '10%',
+        width: '100%',
+        padding: 10,
     },
     image: {
-        width:25,
+        width: 25,
         height: 25,
         marginRight: 10,
         marginTop: 40,
-        marginLeft: 10
+        marginLeft: 10,
     },
     imageBack: {
         flex: 1,
-        width:500,
+        width: 500,
         height: 1000,
-
     },
     btn: {
-        width:100,
+        width: 150,
         height: 50,
-        flexDirection: "row",
-        backgroundColor:"blue",
-        alignItems: "center",
-        justifyContent:"center",
-        marginTop:70,
-        marginLeft: 180
+        backgroundColor: 'blue',
+        alignItems: 'center',
+        justifyContent: 'center',
+        marginTop: 20,
+        marginLeft: 10,
     },
     btntext: {
-        fontSize: 14,
-        color: "white",
-        
-    }
-})
+        fontSize: 16,
+        color: 'white',
+    },
+    modalBackground: {
+        flex: 1,
+        backgroundColor: 'rgba(0, 0, 0, 0.5)',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    modalContainer: {
+        width: 300,
+        padding: 20,
+        backgroundColor: 'white',
+        borderRadius: 10,
+        alignItems: 'center',
+    },
+    modalText: {
+        fontSize: 18,
+        marginBottom: 20,
+    },
+});
+
+export default Carrito;
